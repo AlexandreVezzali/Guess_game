22e0eeda317d72e57ad862e0a325add8715a205f
Ale Vezzali <62294946+AlexandreVezzali@users.noreply.github.com>
2023-01-25 12:33:52 -0300

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,28 @@
+    # i = 1
+    #
+    # while i <= 5:
+    #   print("*" * i)
+    #   i = i + 1
+    # print("Done")
+
+
+
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+
+while guess_count < guess_limit:
+    guess = int(input("Guess: "))
+    guess_count += 1
+    if guess == secret_number:
+        print("You won!")
+        break
+else:
+        print(f"game over! The right number was {secret_number}.")
+
+
+
+
+
+
+
